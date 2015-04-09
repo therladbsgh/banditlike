3f273a530c92102c7cdfbf4a4c74c1da926c45f2
therladbsgh <therladbsgh@gmail.com>
2015-04-09 23:13:16 +0800

    Initial Commit

---
+++ src/banditlike/ApplicationMain.java
@@ -0,0 +1,26 @@
+package banditlike;
+
+import javax.swing.JFrame;
+import asciiPanel.AsciiPanel;
+
+public class ApplicationMain extends JFrame {
+	
+	private static final long serialVersionUID = 1060623638149583738L;
+	
+	private AsciiPanel terminal;
+	
+	public ApplicationMain(){
+		super();
+		terminal = new AsciiPanel(100,32);
+		terminal.write("rl tutorial", 1, 1);
+		add(terminal);
+		pack();
+	}
+	
+	public static void main(String[] args){
+		ApplicationMain app = new ApplicationMain();
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.setVisible(true);
+	}
+
+}
